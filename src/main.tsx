ae47bcdb3b22718768692ff3adc64c9dc9fa6ee4
pkj <pkj2003456@email.com>
2025-01-17 22:21:15 +0800

    完成第一關卡的題目

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+// @ts-ignore
 import store from './store/globalConfig.js'
 import { Provider } from 'react-redux'
 
--- src/pages/ch1-1.tsx
+++ src/pages/ch1-1.tsx
@@ -2,31 +2,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import Quiz from "../components/Quiz";
-import test1Img from "../assets/test1.jpeg";
-import test2Img from "../assets/test2.jpeg";
-import test3Img from "../assets/test3.jpeg";
-import test4Img from "../assets/test4.jpeg";
+import question1 from "../assets/quiz_img/quiz_1/quiz1_1piano.png";
+import question2_ans1 from "../assets/quiz_img/quiz_1/quiz1_2.jpg";
+import question2_ans2 from "../assets/quiz_img/quiz_1/quiz1_2_1.png"; 
+import question2_ans3 from "../assets/quiz_img/quiz_1/quiz1_2_2.png";  
+import question2_ans4 from "../assets/quiz_img/quiz_1/quiz1_2_3.png";  
+import question3_ans1 from "../assets/quiz_img/quiz_1/quiz1_3.png";
+import question3_ans2 from "../assets/quiz_img/quiz_1/quiz1_3_1.png";
+import question3_ans3 from "../assets/quiz_img/quiz_1/quiz1_3_2.png";
+import question3_ans4 from "../assets/quiz_img/quiz_1/quiz1_3_3.png";
 
 const questionsCh1_1 = [
   {
-    questionText: "Which instrument is shown in the picture below?",
-    imageSrc: test1Img, 
+    questionText: "How many keys does a standard piano have?",
+    imageSrc: question1,
     answerOptions: [
-      { answerText: "Piano", isCorrect: true },
-      { answerText: "Guitar", isCorrect: false },
-      { answerText: "Violin", isCorrect: false },
-      { answerText: "Drum", isCorrect: false },
+      { answerText: "61", isCorrect: false },
+      { answerText: "87", isCorrect: false },
+      { answerText: "88", isCorrect: true },
+      { answerText: "89", isCorrect: false },
     ],
   },
   {
-    questionText: "Which of the following is the correct symbol?",
-    imageSrc: test1Img, 
-    answerOptions: [
-      { isImage: true, imageSrc: test1Img, isCorrect: false },
-      { isImage: true, imageSrc: test2Img, isCorrect: true },
-      { isImage: true, imageSrc: test3Img, isCorrect: false },
-      { isImage: true, imageSrc: test4Img, isCorrect: false },
-    ],
+    questionText:"which is the correct gesture to play the piano?",
+    answerOptions:[
+      {isImage:true,imageSrc:question2_ans1,isCorrect:false},
+      {isImage:true,imageSrc:question2_ans4,isCorrect:true},
+      {isImage:true,imageSrc:question2_ans3,isCorrect:false},
+      {isImage:true,imageSrc:question2_ans2,isCorrect:false}
+    ]
   },
   {
     questionText: "What is the name of the white keys on a piano?",
@@ -38,41 +42,24 @@ const questionsCh1_1 = [
     ],
   },
   {
-    questionText: "How many keys does a standard piano have?",
+    questionText: "What is the name of the black keys on a piano?",
     answerOptions: [
-      { answerText: "61", isCorrect: false },
-      { answerText: "76", isCorrect: false },
-      { answerText: "88", isCorrect: true },
-      { answerText: "100", isCorrect: false },
+      { answerText: "Flats", isCorrect: false },
+      { answerText: "Sharps", isCorrect: false },
+      { answerText: "Natural notes", isCorrect: true },
+      { answerText: "Chords", isCorrect: false },
     ],
   },
   {
-    questionText: "Which hand typically plays the melody on a piano?",
-    answerOptions: [
-      { answerText: "Left hand", isCorrect: false },
-      { answerText: "Right hand", isCorrect: true },
-      { answerText: "Both hands equally", isCorrect: false },
-      { answerText: "Neither hand", isCorrect: false },
-    ],
+    questionText: "Which musical note is the correct one?",
+    answerOptions:[
+      {isImage:true,imageSrc:question3_ans1,isCorrect:true},
+      {isImage:true,imageSrc:question3_ans2,isCorrect:false},
+      {isImage:true,imageSrc:question3_ans3,isCorrect:false},
+      {isImage:true,imageSrc:question3_ans4,isCorrect:false}
+    ]
   },
-  {
-    questionText: "What does the sustain pedal on a piano do?",
-    answerOptions: [
-      { answerText: "Makes the notes louder", isCorrect: false },
-      { answerText: "Sustains the sound of the notes", isCorrect: true },
-      { answerText: "Changes the pitch of the notes", isCorrect: false },
-      { answerText: "Stops the sound immediately", isCorrect: false },
-    ],
-  },
-  {
-    questionText: "What is the first note of the C major scale?",
-    answerOptions: [
-      { answerText: "A", isCorrect: false },
-      { answerText: "C", isCorrect: true },
-      { answerText: "E", isCorrect: false },
-      { answerText: "G", isCorrect: false },
-    ],
-  },
+
 ];
 
 function Ch1_1() {
