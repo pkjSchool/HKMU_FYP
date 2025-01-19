32eb1663e686e3e22cda73f4333d115310f12523
Sum <s1333621@live.hkmu.edu.hk>
2025-01-19 13:06:29 +0800

    fix /playing navigate current entry of history stack

--- src/main.tsx
+++ src/main.tsx
@@ -40,7 +40,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/" element={ <App /> } >
             <Route path="/" element={ <LessonPage /> } />
             <Route path="/learning" element={ <LearningPage /> } />
-            <Route path="/self-study" element={ <Navigate to="/playing" /> } />
+            <Route path="/self-study" element={ <Navigate to="/playing" replace={true} /> } />
             <Route path="/task" element={ <TaskPage /> } />
             <Route path="/profile" element={ <ProfilePage /> } /> 
             <Route path="/ai-generation" element={ <AiGenerationPage /> } />
