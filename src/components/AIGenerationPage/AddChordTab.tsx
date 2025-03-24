2aae18b621bada765db8fba26ff28fb15e45093d
cerdragon <117027710+ceruleankenneth@users.noreply.github.com>
2025-03-24 17:53:06 +0800

    start making AIGeneration page

---
+++ src/components/AIGenerationPage/AddChordTab.tsx
@@ -0,0 +1,9 @@
+const AddChordTab = () => {
+    return (
+        <div style={{width: '100vw', height: '80vh'}}>
+            <h1>Add Chord</h1>
+        </div>
+    );
+}
+
+export default AddChordTab;
--- src/pages/LessonPage.tsx
+++ src/pages/LessonPage.tsx
@@ -4,6 +4,10 @@ import LessonMap from '../components/LessonMap';
 import TaskProgress from '../components/TaskProgress';
 import PianoCharacter, {PianoCharacterRef} from '../components/Character/PianoCharacter.tsx';
 
+import axios from 'axios';
+
+import { api_piano_transcribe } from '../api_request/request.tsx';
+
 // import { showCharacter, hideCharater, setMessage, changePosition } from '../store/pianoCharacherSlice';
 // import { AppDispatch, RootState } from '../store/globalConfig';
 
@@ -49,12 +53,41 @@ function App() {
     pianoCharacterRef.current?.changePositionHandler({ right: "50px", bottom: "0px" });
   }, []);
 
+
+  const testAPI = async () => {
+    // Fetch the file from the URL
+    const response = await fetch("http://localhost:5173/test.wav");
+    if (!response.ok) {
+      throw new Error(`Failed to fetch file: ${response.statusText}`);
+    }
+
+    // Convert the response to a Blob
+    const blob = await response.blob();
+
+    var formData = new FormData();
+    formData.append("audio", blob, "test.wav");
+
+    api_piano_transcribe(formData).then((response) => {
+      console.log(response);
+    });
+  }
+
   return (
     <div className="lesson-index-container">
         <button onClick={() => pianoCharacterRef.current?.showCharacterHandler()}>Show Character</button>
         <button onClick={() => pianoCharacterRef.current?.hideCharacterHandler()}>Hide Character</button>
         <button onClick={() => pianoCharacterRef.current?.setMessageHandler(`Hello, ${Math.random().toFixed(3)}! Welcome back!`)}>Set Message</button>
         <button onClick={() => pianoCharacterRef.current?.changePositionHandler({ right: Math.floor(Math.random() * (100 - 1) + 1), bottom: Math.floor(Math.random() * (100 - 1) + 1) })}>Change Position</button>
+        <input type="file" accept=".wav" onChange={(e) => {
+          const file = e.target.files?.[0];
+          if (file) {
+            var formData = new FormData();
+            formData.append("audio", file);
+            api_piano_transcribe(formData).then((response) => {
+              console.log(response);
+            });
+          }
+        }} />
         <LessonMap 
           chapters={sampleChapters.map(chapter => ({
             ...chapter,
